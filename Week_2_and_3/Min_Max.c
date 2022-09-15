de7330597ce9bd00de9bdd2cba1d46e16b0915e6
Sina <sina.t.mec@gmail.com>
2022-09-14 21:17:57 -0400

    Add files via upload

---
+++ Week_2_and_3/Min_Max.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <limits.h> //This header is used give the maximum and minimum sizes of the various integers
+//The values you get from this code depents on the machine architecture
+int main(void)
+{
+	//with unsigned keywork all the bits are used to store the number and the first bit is not used to store the sign!
+	unsigned long I_num = ULONG_MAX;
+
+	printf("maximum int = %i\n, ", INT_MAX);
+	printf("minimum int = %i\n", INT_MIN); 
+	printf("maximum unsigned = %u\n", UINT_MAX); 
+	printf("maximum long int = %li\n", LONG_MAX); 
+	printf("maximum unsigned long = %lu\n", I_num); 
+
+	return 0;
+
+}
