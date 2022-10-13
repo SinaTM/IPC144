b1db1366ed3bfe6c59d1fd7f5f621a42ef31a838
Sina <sina.t.mec@gmail.com>
2022-10-13 13:29:10 -0400

    Update Learn_1.c

--- Week_2_and_3/Learn_1.c
+++ Week_2_and_3/Learn_1.c
@@ -5,18 +5,17 @@
 int main(void)
 {
 
-	int A, B;	//define two ineger variables
-	
+	int num_1, num_2;	//define two ineger variables
+
 	printf("Enter a float value:\n");
 	/* get a float numberand store the whole part(integral part) in A and the fractional part
 	in B as two integer numbers*/
-	scanf("%d.%d", &A, &B);
-	printf("\nValue of A and B are, %d and %d\n", A, B);
-	// +------------------------------------------------------
+	scanf("%d.%d", &num_1, &num_2);
+	printf("\nValue of A and B are, %d and %d\n", num_1, num_2);
 	
-
 	int Year, Month, Day;
 	printf("Please input the date (Year/Month/Day)\n");
+
 	//get input from user in a specific format and storing the values as integers in variables
 	scanf("%d/%d/%d", &Year, &Month, &Day);
 	//incrementing Day by one (won't always have a logical outcome!)
@@ -25,9 +24,6 @@ int main(void)
 	printf("\nTomorow\'s date is: %d/%d/%d \n", Year, Month, Day);
 	printf("=====================================\n");
 
-	
-	
-	
 
-	return 0; 
+	return 0;
 }
