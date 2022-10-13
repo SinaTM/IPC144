df272d0e2b921f5dcceafe78747792e4327a1b79
Sina <sina.t.mec@gmail.com>
2022-10-13 13:26:04 -0400

    Update Formatting_2.c

--- Week_2_and_3/Formatting_2.c
+++ Week_2_and_3/Formatting_2.c
@@ -3,12 +3,11 @@
 
 int main(void)
 {
-	int Int_1 = 21;
-	float FL_1 = 342.92f;
-	printf("|%d|%6d|%-6d|%6.3d|\n", Int_1, Int_1, Int_1, Int_1);
-	printf("|%9.3f|%9.3e|%-9g|%-9.3e|%-9.3f|\n", FL_1, FL_1, FL_1, FL_1, FL_1);
+	int int1 = 21;
+	float fl1 = 342.92f;
+	printf("|%d|%6d|%-6d|%6.3d|\n", int1, int1, int1, int1);
+	printf("|%9.3f|%9.3e|%-9g|%-9.3e|%-9.3f|\n", fl1, fl1, fl1, fl1, fl1);
 
-	return 0; 
-
+	return 0;
 
 }
