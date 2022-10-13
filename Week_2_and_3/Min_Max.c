56851999bd6f5b73e3d413dcc14444188f69a18e
Sina <sina.t.mec@gmail.com>
2022-10-13 13:30:03 -0400

    Update Min_Max.c

--- Week_2_and_3/Min_Max.c
+++ Week_2_and_3/Min_Max.c
@@ -4,13 +4,13 @@
 int main(void)
 {
 	//with unsigned keywork all the bits are used to store the number and the first bit is not used to store the sign!
-	unsigned long I_num = ULONG_MAX;
+	unsigned long num = ULONG_MAX;
 
 	printf("maximum int = %i\n, ", INT_MAX);
-	printf("minimum int = %i\n", INT_MIN); 
-	printf("maximum unsigned = %u\n", UINT_MAX); 
-	printf("maximum long int = %li\n", LONG_MAX); 
-	printf("maximum unsigned long = %lu\n", I_num); 
+	printf("minimum int = %i\n", INT_MIN);
+	printf("maximum unsigned = %u\n", UINT_MAX);
+	printf("maximum long int = %li\n", LONG_MAX);
+	printf("maximum unsigned long = %lu\n", num);
 
 	return 0;
 
